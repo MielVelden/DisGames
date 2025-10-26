e3a2e6c2576b7d672e93e0b28e74ccc06342c657
MielVelden <mielvelden@gmail.com>
2025-10-26 17:20:20 +0100

    Remove deprecated files

--- functions/general/permissionManager.js
+++
@@ -1,7 +0,0 @@
-module.exports.checkPermission = async (functions, message, permission) => {
-    functions.createLog("Checking permissions", false, true);
-    const permsFor = message.guild.members.me.permissions;
-    if (permsFor === null || permsFor === undefined) return false;
-    if (!permsFor.has(permission)) return false;
-    return true;
-};
